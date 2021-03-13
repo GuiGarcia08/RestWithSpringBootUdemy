a3466fa56e6fc1fb268d40ac9542b93d8169f2e5
GuilhermeGarcia <guigarcialo22@gmail.com>
2021-03-13 14:38:43 -0300

    CORS SpringBoot and REST Udemy

---
+++ 12RestWithSpringBootUdemyCORS/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+(1,'Rio Preto - SP','Guilherme','Male','Garcia'),
+(2,'Rio Preto - SP','Loyane','Female','Flores Garcia'),
+(3,'Rio Preto - SP','Alice','Female','Garcia');
