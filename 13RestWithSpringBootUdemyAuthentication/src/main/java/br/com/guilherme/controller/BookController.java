8575eba834028520c62a012395019428010fb573
GuilhermeGarcia <guigarcialo22@gmail.com>
2021-03-16 08:24:36 -0300

    Authentication Token SpringBoot and REST Udemy

---
+++ 13RestWithSpringBootUdemyAuthentication/src/main/java/br/com/guilherme/controller/BookController.java
@@ -0,0 +1,70 @@
+package br.com.guilherme.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.guilherme.data.vo.v1.BookVO;
+import br.com.guilherme.services.BookServices;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "Book Endpoint", description = "Description for Book", tags = { "Book Endpoint" })
+@RestController
+@RequestMapping("/api/book/v1")
+public class BookController {
+
+	@Autowired
+	private BookServices service;
+
+	@ApiOperation(value = "Find All Books")
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
+	public List<BookVO> findAll() {
+		List<BookVO> books = service.findAll();
+		books.stream().forEach(p -> p.add(linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()));
+		return books;
+	}
+
+	@ApiOperation(value = "Find specific Book by your ID")
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
+	public BookVO findById(@PathVariable("id") Long id) {
+		BookVO bookVO = service.findById(id);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		return bookVO;
+	}
+
+	@ApiOperation(value = "Create a new Book")
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public BookVO create(@RequestBody BookVO book) {
+		BookVO bookVO = service.create(book);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+		return bookVO;
+	}
+
+	@ApiOperation(value = "Update a specific Book ")
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yaml" }, consumes = {
+			"application/json", "application/xml", "application/x-yaml" })
+	public BookVO update(@RequestBody BookVO book) {
+		BookVO bookVO = service.update(book);
+		bookVO.add(linkTo(methodOn(BookController.class).findById(bookVO.getKey())).withSelfRel());
+		return bookVO;
+	}
+
+	@ApiOperation(value = "Delete specific Book by your ID")
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		service.delete(id);
+	}
+}
---
+++ 13RestWithSpringBootUdemyAuthentication/src/main/java/br/com/guilherme/services/PersonServices.java
@@ -0,0 +1,55 @@
+package br.com.guilherme.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.guilherme.converter.DozerConverter;
+import br.com.guilherme.data.model.Person;
+import br.com.guilherme.data.vo.v1.PersonVO;
+import br.com.guilherme.exception.ResourceNotFoundException;
+import br.com.guilherme.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+
+	@Autowired
+	PersonRepository repository;
+
+	public PersonVO create(PersonVO person) {
+		var entity = DozerConverter.parseObject(person, Person.class);
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+
+	public List<PersonVO> findAll() {
+		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+
+	public PersonVO findById(Long id) {
+
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
+
+	public PersonVO update(PersonVO person) {
+		var entity = repository.findById(person.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+		return vo;
+	}
+
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
+	}
+}
