d0e36558439d3586bf298082313213da46fb26a8
GuilhermeGarcia <guigarcialo22@gmail.com>
2021-03-20 12:36:31 -0300

    FrontEnd with React Udemy

---
+++ 19RestWithSpringBootUdemyReact/client/src/pages/Books/index.js
@@ -0,0 +1,105 @@
+import React, {useState, useEffect} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiPower, FiEdit, FiTrash2} from 'react-icons/fi';
+import api from '../../services/api';
+import './styles.css';
+
+import logoImage from "../../assets/logo.svg";
+
+export default function Books(){
+
+    const [books, setBooks] = useState([]);
+    const [page, setPages] = useState(1);
+
+    const username = localStorage.getItem('username');
+    const accessToken = localStorage.getItem('accessToken');
+
+    const history = useHistory();
+
+    async function logout(){
+        localStorage.clear();
+        history.push('/')
+    }
+
+    async function editBook(id){
+        try {
+            history.push(`book/new/${id}`);
+        } catch (err) {
+            alert('Edit failed! Try Again!')
+        }
+    }
+
+    async function deleteBook(id){
+        try {
+            await api.delete(`api/book/v1/${id}`,{ 
+                headers:{
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+
+            setBooks(books.filter(book => book.id !== id))
+        } catch (err) {
+            alert('Delete Falied! Try Again')
+        }
+    }
+
+    async function fetchMoreBooks(){
+        const response = await api.get('api/book/v1',{
+            headers:{
+                Authorization: `Bearer ${accessToken}`
+            },
+            params:{
+                page : page,
+                limit : 4,
+                direction : 'asc',
+            }
+        });
+
+        setBooks([...books, ...response.data_embebed.bookVO])
+        setPages(page + 1);
+    }
+
+    useEffect(() => {
+        fetchMoreBooks();   
+    }, []);
+
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Guilherme"/>
+                <span>Welcome, <strong>{username.toUpperCase()}</strong>!</span>
+                <Link className="button" to='book/new/0'>Add new book</Link>
+                <button onclikck={logout} type="button">
+                    <FiPower size ={18} color="#251FC5" />
+                </button>
+            </header>
+
+            <h1>Registered Books</h1>
+            <ul>
+                {books.map(book =>(
+                    <li key={book.id}>
+                        <strong>Title:</strong>
+                        <p>{book.title}</p>
+                        <strong>Author:</strong>
+                        <p>{book.author}</p>
+                        <strong>Prico:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency:'BRL'}).format(book.price)}</p>
+                        <strong>Realease Date:</strong>
+                        <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
+
+                        <buton onclikck={()=> editBook(book.id)} type="button">
+                            <FiEdit size={20} color="#251FC5"/>
+                        </buton>
+
+                        <buton onclikck={() => deleteBook(book.id)} type="button">
+                            <FiTrash2 size={20} color="#251FC5"/>
+                        </buton>
+
+                    </li>
+                ))}
+            </ul>
+
+            <button classname="button" onclikck={fetchMoreBooks} type="button">Load More</button>
+        </div>
+    );
+}
---
+++ 19RestWithSpringBootUdemyReact/client/src/pages/Login/index.js
@@ -0,0 +1,59 @@
+import React,{useState} from 'react';
+import {useHistory} from 'react-router-dom'
+import './styles.css';
+import api from '../../services/api';
+
+import padlock from "../../assets/padlock.png";
+import logoImage from "../../assets/logo.svg";
+
+export default function Login(){
+
+    const {username, setUsername} = useState('');
+    const {password, setPassword} = useState('');
+
+    const history = useHistory();
+
+    async function login(e){
+        e.preventDefault();
+
+        const data ={
+            username,
+            password,
+        };
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('accessToken', response.data.token);
+
+            history.push('/books');
+        } catch (err) {
+            alert('Login failed! Try agains!');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Guilherme Logo"/>
+                <form onSubmit={login}>
+                    <h1>Access your Account</h1>
+                    <input
+                        placeholder="Username"
+                        value = {username}
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password" placeholder="Password"
+                        value = {password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className="button" type="submit">Login</button>
+                </form>
+            </section>
+
+            <img src={padlock} alt="Login"/>
+        </div>
+    );
+}
